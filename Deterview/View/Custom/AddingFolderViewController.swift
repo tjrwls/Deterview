bb4983f6a058844d6dc673b294c9fe4ec3877e9a
mumjee03 <mumje03@gmail.com>
2023-02-11 18:16:17 +0900

    [Refactor] 볼필요한 코드 제거 및 주석 추가 [Refactor] UIBarbuttonItem 코드 수정

--- Deterview/View/Custom/AddingFolderViewController.swift
+++ Deterview/View/Custom/AddingFolderViewController.swift
@@ -7,83 +7,70 @@
 
 import UIKit
 
-class AddingFolderViewController: UIViewController {
-    var questionStore: QuestionFolderStore? = nil
-    var viewController: CustomViewController? = nil
-    var customListViewController: CustomListViewController? = nil
-    var folderId: String = ""
-    var folderNameTextLength: Int {
-        folderNameTextField.text?.count ?? 0
-    }
-    
+final class AddingFolderViewController: UIViewController {
     @IBOutlet weak var folderNameTextField: UITextField!
-    @IBOutlet weak var cancleBtn: UIButton!
+    @IBOutlet weak var cancelBtn: UIButton!
     @IBOutlet weak var modalView: UIView!
-    @IBOutlet weak var saveFolderNameBtn: UIButton!
+    @IBOutlet weak var folderNameSaveBtn: UIButton!
     @IBOutlet weak var guideText: UILabel!
     @IBOutlet weak var limitTextLengthMessage: UILabel!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        modalView.layer.cornerRadius = CGFloat(10)
-        if viewController != nil{
-            guideText.text = "폴더 이름을 입력해주세요"
-            limitTextLengthMessage.text = "1~9자 이내로 입력해주세요."
-            limitTextLengthMessage.font = .systemFont(ofSize: 14)
-            limitTextLengthMessage.textColor = .gray
-            saveFolderNameBtn.isEnabled = false
-        } else {
-            guideText.text = "질문을 입력해주세요."
-            limitTextLengthMessage.text = ""
-            saveFolderNameBtn.isEnabled = false
-        }
-        
-        guideText.textAlignment = .center
-        cancleBtn.setTitle("취소", for: .normal)
-        saveFolderNameBtn.setTitle("완료", for: .normal)
-        saveFolderNameBtn.backgroundColor = UIColor(.customGray)
-        saveFolderNameBtn.setTitleColor(.systemGray3, for: .normal)
-        saveFolderNameBtn.layer.cornerRadius = 5
-        saveFolderNameBtn.titleLabel?.font = .systemFont(ofSize: 16)
-        
+    var folderId: String?
+    private var folderNameTextLength: Int {
+        folderNameTextField.text?.count ?? 0
     }
     
-    // MARK: 뷰생성시 키보드 밑 View 올리기
+    var questionStore: QuestionFolderStore?
+    var customViewController: CustomViewController?
+    var customListViewController: CustomListViewController?
+    
+    // MARK: - Lifecycle Methods
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        configureUI()
+    }
+    
+    /// 뷰생성시 키보드 밑 View 올리기
     override func viewWillAppear(_ animated: Bool) {
         self.folderNameTextField.becomeFirstResponder()
         UIView.animate( // 키보드 올라올 때
-            withDuration: 0.1
-            , animations: {
-                if UIDevice.current.orientation.isLandscape{
+            withDuration: 0.1,
+            animations: {
+                if UIDevice.current.orientation.isLandscape {
                     self.view.transform = CGAffineTransform(translationX: 0, y: -150) // view 위로 밀림
-                }else{
+                } else {
                     self.view.transform = CGAffineTransform(translationX: 0, y: -300)
                 }
             })
     }
-    // MARK: 뷰회전시 View 위치 재조정
+    
+    /// 뷰회전시 View 위치 재조정
     override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
-        if UIDevice.current.orientation.isLandscape{
+        if UIDevice.current.orientation.isLandscape {
             self.view.transform = CGAffineTransform(translationX: 0, y: -150)
-        }else{
+        } else {
             self.view.transform = CGAffineTransform(translationX: 0, y: -300)
         }
     }
     
+    // MARK: - Methods
     @IBAction func tapSaveBtn(_ sender: Any) {
-        if viewController != nil {
+        if customViewController != nil {
             let questionFolder: QuestionFolder = QuestionFolder()
             questionFolder.folderName = folderNameTextField.text ?? ""
             questionFolder.category = "Custom"
             questionStore?.createdQuestionFolder(questionFolder)
             questionStore?.readQuestionFolder()
+            
             self.dismiss(animated: true) {
                 UICollectionView.performWithoutAnimation {
-                    self.viewController?.customCollectionView.reloadSections([0])
+                    self.customViewController?.customCollectionView.reloadSections([0])
                 }
             }
         } else {
-            questionStore?.createdQuestion(id: folderId, addQuestion: folderNameTextField.text ?? "")
+            questionStore?.createdQuestion(id: folderId ?? "", addQuestion: folderNameTextField.text ?? "")
+            
             self.dismiss(animated: true) {
                 UITableView.performWithoutAnimation {
                     self.customListViewController?.customListView.reloadData()
@@ -92,40 +79,69 @@ class AddingFolderViewController: UIViewController {
         }
     }
     
-    @IBAction func tabCancleBtn(_ sender: Any) {
+    @IBAction func tabCancelBtn(_ sender: Any) {
         self.dismiss(animated: true)
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    
     @IBAction func textFieldEditingChanged(_ sender: Any) {
-        if viewController != nil { // 폴더 추가일 경우
+        if customViewController != nil { // 폴더 추가일 경우
             if folderNameTextLength > 0 && folderNameTextLength < 10 {
-                saveFolderNameBtn.isEnabled = true
-                saveFolderNameBtn.backgroundColor = UIColor(.mainColor)
-                saveFolderNameBtn.setTitleColor(.white, for: .normal)
+                folderNameSaveBtn.isEnabled = true
+                folderNameSaveBtn.backgroundColor = UIColor(.mainColor)
+                folderNameSaveBtn.setTitleColor(.white, for: .normal)
                 
             } else {
-                saveFolderNameBtn.isEnabled = false
-                saveFolderNameBtn.backgroundColor = UIColor(.customGray)
-                saveFolderNameBtn.setTitleColor(.systemGray3, for: .normal)
+                folderNameSaveBtn.isEnabled = false
+                folderNameSaveBtn.backgroundColor = UIColor(.customGray)
+                folderNameSaveBtn.setTitleColor(.systemGray3, for: .normal)
             }
         } else { // 질문 추가일 경우
-            if folderNameTextLength > 0  {
-                saveFolderNameBtn.isEnabled = true
-                saveFolderNameBtn.backgroundColor = UIColor(.mainColor)
-                saveFolderNameBtn.setTitleColor(.white, for: .normal)
+            if folderNameTextLength > 0 {
+                folderNameSaveBtn.isEnabled = true
+                folderNameSaveBtn.backgroundColor = UIColor(.mainColor)
+                folderNameSaveBtn.setTitleColor(.white, for: .normal)
             } else {
-                saveFolderNameBtn.isEnabled = false
-                saveFolderNameBtn.backgroundColor = UIColor(.customGray)
-                saveFolderNameBtn.setTitleColor(.systemGray3, for: .normal)
+                folderNameSaveBtn.isEnabled = false
+                folderNameSaveBtn.backgroundColor = UIColor(.customGray)
+                folderNameSaveBtn.setTitleColor(.systemGray3, for: .normal)
             }
         }
+    }
+    
+    private func configureUI() {
+        modalView.layer.cornerRadius = CGFloat(10)
+        guideText.textAlignment = .center
+        cancelBtn.setTitle("취소", for: .normal)
+        configureSaveFolderNameBtn()
+        configureCheckingViewController()
+    }
+    
+    private func configureCheckingViewController() {
+        if customViewController != nil {
+            configureAddingFolderUI()
+        } else {
+            configureAddingQuestionUI()
+        }
+    }
+    
+    private func configureAddingFolderUI() {
+        guideText.text = "폴더 이름을 입력해주세요"
+        limitTextLengthMessage.text = "1~9자 이내로 입력해주세요."
+        limitTextLengthMessage.font = .systemFont(ofSize: 14)
+        limitTextLengthMessage.textColor = .gray
+    }
+    
+    private func configureAddingQuestionUI() {
+        guideText.text = "질문을 입력해주세요."
+        limitTextLengthMessage.text = ""
+    }
+    
+    private func configureSaveFolderNameBtn() {
+        folderNameSaveBtn.setTitle("완료", for: .normal)
+        folderNameSaveBtn.backgroundColor = UIColor(.customGray)
+        folderNameSaveBtn.setTitleColor(.systemGray3, for: .normal)
+        folderNameSaveBtn.layer.cornerRadius = 5
+        folderNameSaveBtn.titleLabel?.font = .systemFont(ofSize: 16)
+        folderNameSaveBtn.isEnabled = false
     }
 }
